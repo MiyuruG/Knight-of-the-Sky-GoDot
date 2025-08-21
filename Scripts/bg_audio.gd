e458b27844227aa70ab3402f428441945de4a69d
MiyuruG <149800968+MiyuruG@users.noreply.github.com>
2025-08-22 02:00:10 +0530

    Add files via upload

--- Scripts/bg_audio.gd
+++ Scripts/bg_audio.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
 func _process(_delta: float) -> void:
-	pass
+	if !$AudioStreamPlayer2D.playing:
+		$AudioStreamPlayer2D.play()
--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,20 +1,9 @@
 extends Area2D
-<<<<<<< HEAD
 @onready var timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died")
 	timer.start()
 
-=======
-
-@onready var timer = $Timer
-
-func _on_body_entered(body):
-	print("You died!")
-	timer.start()
-
-
->>>>>>> 6f10010 (Added stuff)
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
